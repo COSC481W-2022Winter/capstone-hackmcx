9982d2d819244215cf7ab592f41e73d6762cde3b
3dxp <43457915+3dxp@users.noreply.github.com>
2022-01-22 17:36:41 -0500

    Tech Demo
    Setup MySQL + Express + React demo.

---
+++ packages/hackmcx-api/src/controllers/example.controller.js
@@ -0,0 +1,11 @@
+import {pool} from '../clients/mariadb.js'
+
+export async function getExample(req, res){
+    pool.getConnection().then(connection => {
+        connection.query('SELECT CURRENT_TIMESTAMP;')
+            .then((response) => {
+                res.send(response)
+            })
+        }
+    );
+}
