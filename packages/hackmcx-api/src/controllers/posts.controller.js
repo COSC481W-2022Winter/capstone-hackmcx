ae515699288879e51f97eb52459acc88d3f567df
3dxp <43457915+3dxp@users.noreply.github.com>
2022-02-19 18:24:30 -0500

    feat: Update POST Post Error Handling
    Update error handling for POSTing a post.
    Return errors on the following conditions:
    - Missing image URL
    - Invalid image URL
    - Missing title
    - Blank title

--- packages/hackmcx-api/src/controllers/posts.controller.js
+++ packages/hackmcx-api/src/controllers/posts.controller.js
@@ -25,16 +25,28 @@ export async function getPostById(req, res){
 export async function postPost(req, res){
     const imageURL = req.body.imageURL;
     const regex = /(https?:\/\/.*\.(?:png|jpg|gif|svg))/i;
-    const isPicture = imageURL.match(regex)[0] === imageURL;
-    const emptyTitle = /^\s*$/.test(req.body.title);
+    const isPicture = imageURL && imageURL.match(regex)[0] === imageURL;
+    const emptyTitle = req.body.title && /^\s*$/.test(req.body.title);
 
-    if(isPicture && !emptyTitle) {
-        dbClient
-            .insert(req.body).into('posts')
-            .then(result => {
-                res.statusCode = 201
-                // TODO: Set location header... res.header('Location', req.)
-                res.send(result)
-            })
+    if(!isPicture){
+        res.statusCode = 400;
+        res.send({
+            error: "Invalid image URL: Image url is either missing or invalid."
+        })
     }
+
+    if (emptyTitle){
+        res.statusCode = 400;
+        res.send({
+            error: "Title cannot be missing or blank."
+        })
+    }
+
+    dbClient
+        .insert(req.body).into('posts')
+        .then(result => {
+            res.statusCode = 201
+            // TODO: Set location header... res.header('Location', req.)
+            res.send(result)
+        })
 }
