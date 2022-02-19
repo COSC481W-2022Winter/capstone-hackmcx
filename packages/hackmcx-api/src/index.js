8d4103823c93ae227be9a1f72f54ea5206d5ba7b
Zhaoyue Xu <zxu5@emich.edu>
2022-02-19 17:27:37 -0500

    feat: Check to create schema for migration
    Added a mysql call to create the schema if it does not exists so knex tables can be created

--- packages/hackmcx-api/src/index.js
+++ packages/hackmcx-api/src/index.js
@@ -4,12 +4,29 @@ import {fileURLToPath} from 'url'
 import {app} from './app.js'
 import {dbClient} from "./db/db.js";
 
+import mysql from 'mysql2'
+
 let port = process.env.PORT || 3003;
 
 if (process.argv[2])
 {
     if (process.argv[2] === '--migrate') {
         console.info("Running migrations!")
+        
+        const con = mysql.createConnection({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            });
+        con.connect(function(err) {
+            if (err) throw err;
+            con.query("create database if not exists app", function (err, results){
+                con.end();
+                if(err) throw err;
+            });
+        });       
+
         await dbClient.migrate.latest({ directory: dirname(fileURLToPath(import.meta.url))+'/db/migrations'})
             .then(([batchNo, log]) => {
                 if (!log.length){
