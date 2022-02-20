94488b87beab71e2f6195efcb7edf02d0e3d54bd
3dxp <43457915+3dxp@users.noreply.github.com>
2022-02-20 16:05:07 -0500

    fix: Fix Race Condition in DB Migration Runner
    This change moves the code to promises that can be awaited to make sure
    the schema is present before the migrations actually run for the app.
    The app name is also taken from the configuration env file instead of
    being hard coded to make sure everything runs successfully.
    
    #26

--- packages/hackmcx-api/src/index.js
+++ packages/hackmcx-api/src/index.js
@@ -3,7 +3,6 @@ import {dirname} from 'path'
 import {fileURLToPath} from 'url'
 import {app} from './app.js'
 import {dbClient} from "./db/db.js";
-
 import mysql from 'mysql2'
 
 let port = process.env.PORT || 3003;
@@ -19,13 +18,12 @@ if (process.argv[2])
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
             });
-        con.connect(function(err) {
-            if (err) throw err;
-            con.query("create database if not exists app", function (err, results){
-                con.end();
-                if(err) throw err;
-            });
-        });       
+        await con.promise().connect();
+        // NOTE: Create database does not allow for parameterization of the database name for whatever reason.
+        // Please do not ever interpolate a DB query string EVER as it leaves the app vulnerable to SQL injection
+        // attacks.
+        await con.promise().query(`create database if not exists ${process.env.DB_NAME}`);
+        await con.promise().destroy();
 
         await dbClient.migrate.latest({ directory: dirname(fileURLToPath(import.meta.url))+'/db/migrations'})
             .then(([batchNo, log]) => {
@@ -35,11 +33,12 @@ if (process.argv[2])
                     console.info(`Ran migrations : ${log.join(', ')}`)
                 }
         });
-        dbClient.destroy().then()
+        await dbClient.destroy()
     } else if (process.argv[2] === '--seed') {
         await dbClient.seed.run({directory: dirname(fileURLToPath(import.meta.url))+'/db/seeds'}).then(_ => {
             console.log("Seed data was applied.")
         });
+        await dbClient.destroy();
     }
 } else{
     app.listen(port, () => console.log(`'${process.env.NODE_ENV}' server  is running on port: '${port}'.`))
