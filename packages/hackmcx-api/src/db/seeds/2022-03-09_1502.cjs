e08a0c2d19c0babbada6586c0dac86f9610072c4
Zhaoyue Xu <zxu5@emich.edu>
2022-03-09 15:40:34 -0500

    feat: Add Migrations and Seed for Captions
    Added the migration for creation of captions table
    and added Caption seed data
    
    #60

---
+++ packages/hackmcx-api/src/db/seeds/2022-03-09_1502.cjs
@@ -0,0 +1,49 @@
+function seed(knex) {
+    return knex('captions').then(_ =>
+        knex('captions').insert([
+            {
+                id: 1,
+                caption: 'Caption 1',
+                post_id: 1,
+                average_rating: 3.0,
+                number_of_ratings: 2,
+            },
+            {
+                id: 2,
+                caption: 'Caption 2',
+                post_id: 1,
+                average_rating: 3.3,
+                number_of_ratings: 100,
+            },
+            {
+                id: 3,
+                caption: 'Caption 3',
+                post_id: 2,
+                average_rating: 3.6,
+                number_of_ratings: 5,
+            },
+            {
+                id: 4,
+                caption: 'Caption 4',
+                post_id: 2,
+                average_rating: 1.0,
+                number_of_ratings: 2,
+            },
+            {
+                id: 5,
+                caption: 'Caption 5',
+                post_id: 3,
+                average_rating: 2.4,
+                number_of_ratings: 10,
+            },
+            {
+                id: 6,
+                caption: 'Caption 6',
+                post_id: 3,
+                average_rating: 3.5,
+                number_of_ratings: 2,
+            },
+        ]));
+}
+
+module.exports = { seed }
