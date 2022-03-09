327dbeeb3c7bc6611e182a22b14b919b7359d14d
Alec Campbell <acampb65@emich.edu>
2022-03-09 18:56:56 -0500

    fix: Make Names Nullable, Add Default Image
    Removed notNullable condition from first and last name.
    Added default value for images.

--- packages/hackmcx-api/src/db/migrations/2022-03-09_1604_create_table_users.cjs
+++ packages/hackmcx-api/src/db/migrations/2022-03-09_1604_create_table_users.cjs
@@ -1,10 +1,10 @@
 function up (knex) {
     return knex.schema
         .createTable('users', function (table){
-            table.string('username', 255).primary().notNullable();
-            table.string('first_name', 255).notNullable();
-            table.string('last_name', 255).notNullable();
-            table.string('imageUrl', 2048).notNullable();
+            table.string('username', 255).primary();
+            table.string('first_name', 255);
+            table.string('last_name', 255);
+            table.string('imageUrl', 2048).notNullable().defaultTo('https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png');
             table.boolean('disable').defaultTo(false);
             table.timestamp('createdAt')
         });
