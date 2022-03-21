4e01cdcee0bd23566ea17c3f6a0d9b822f12d7d5
Zhaoyue Xu <zxu5@emich.edu>
2022-03-21 01:19:05 -0400

    fix: group by query incorrect
    updated query to correctly only return one copy of each post
    
    #79

--- packages/hackmcx-api/src/controllers/posts.controller.js
+++ packages/hackmcx-api/src/controllers/posts.controller.js
@@ -2,14 +2,19 @@ import {dbClient} from '../db/db.js'
 
 export async function getPosts(req, res){
     var finalResults;
+    var top
     await dbClient
-        .select('title', 'imageUrl', 'posts.createdAt', 'posts.id', 'caption')
+        .select('title', 'imageUrl', 'posts.createdAt', 'posts.id', 'topPost.caption')
         .from('posts')
-        .leftOuterJoin('captions as c', function() {
-          this.on('posts.id', '=', 'post_id')
-          this.andOn('average_rating', '=', dbClient.raw('(select max(??) from ?? where ??=??)', ['average_rating', 'captions', 'post_id', 'c.post_id']))
+        .leftJoin(
+            dbClient
+            .select(dbClient.raw('post_id, caption, max(average_rating)'))
+            .from('captions')
+            .groupBy('post_id')
+            .as('topPost')
+        , function(){
+            this.on('posts.id', '=', 'topPost.post_id')
         })
-        .groupBy('posts.id')
         .then(results => {finalResults = results})
 
     finalResults.forEach(function (post, index) {
