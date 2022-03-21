67133afa8a799ea315b38e9a155c409f69640dcc
Zhaoyue Xu <zxu5@emich.edu>
2022-03-21 00:06:19 -0400

    fix: remove duplicate posts with matching average
    Duplicate posts are listed if there are captions
    with same max average rating
    
    #79

--- packages/hackmcx-api/src/controllers/posts.controller.js
+++ packages/hackmcx-api/src/controllers/posts.controller.js
@@ -9,6 +9,7 @@ export async function getPosts(req, res){
           this.on('posts.id', '=', 'post_id')
           this.andOn('average_rating', '=', dbClient.raw('(select max(??) from ?? where ??=??)', ['average_rating', 'captions', 'post_id', 'c.post_id']))
         })
+        .groupBy('posts.id')
         .then(results => {finalResults = results})
 
     finalResults.forEach(function (post, index) {
