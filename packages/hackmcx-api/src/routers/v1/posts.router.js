0e0a2aa98186f5ae9118fbbc63b97dd34e4a408b
3dxp <43457915+3dxp@users.noreply.github.com>
2022-03-30 21:47:31 -0400

    feat: Add Token Authentication
    Add token authentication middleware.
    Add authentication to create post.
    
    #87

--- packages/hackmcx-api/src/routers/v1/posts.router.js
+++ packages/hackmcx-api/src/routers/v1/posts.router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {router as captionsRouter} from "./posts/captions.router.js"
 import {getPostById, getPosts, postPost} from "../../controllers/posts.controller.js";
+import {authenticateToken} from "../../middleware/auth.middleware.js";
 
 export const router = new express.Router();
 
 router.get('/', getPosts)
-router.post('/', postPost)
+router.post('/', authenticateToken, postPost)
 
 router.get('/:postId', getPostById)
 
