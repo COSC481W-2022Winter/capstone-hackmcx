f1391e12fe25b5f9524f71d77bd4210b73082e5d
Wyatt Curtis <90487870+toxictortoise@users.noreply.github.com>
2022-04-01 14:55:50 -0400

    feat: Add Login page layout and front-end validation

--- packages/hackmcx-client/src/App.js
+++ packages/hackmcx-client/src/App.js
@@ -7,6 +7,7 @@ import AppNav from "./AppNav";
 import Grid from "@material-ui/core/Grid";
 import UserProfile from './components/UserProfile';
 import Post from "./components/posts/Post";
+import Login from './components/Login'
 
 export default function App() {
 	return (
@@ -23,6 +24,7 @@ export default function App() {
 							<Route exact path="/user/:userId" element={<UserProfile/>}/>
 							<Route exact path="/posts/:postId" element={<Post/>}/>
 							<Route exact path="/error/404" element={<ErrorPage/>}/>
+							<Route exact path="/login" element={<Login/>}/>
 						</Routes>
 					</Grid>
 				</Grid>
---
+++ packages/hackmcx-client/src/components/Login.js
@@ -0,0 +1,146 @@
+import React from "react";
+import { Avatar, Grid, TextField, Button } from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+import { useState } from 'react';
+
+
+const Login = () =>  {
+    const useStyles = makeStyles((theme) => ({
+      root: {
+        alignSelf: 'center',
+        justifyContent: "center",
+        alignItems: "center",
+        display: 'flex',
+        '& > *': {
+          margin: theme.spacing(1),
+        },
+      },
+      input: {
+        display: "none",
+      },
+      large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+      }
+
+    }));
+
+    const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [usernameHelper, setUsernameHelper] = useState("Username cannot be empty.");
+	const [passwordHelper, setPasswordHelper] = useState("Password cannot be empty.");
+	const [passwordError, setPasswordError] = useState(true);
+	const [usernameError, setUsernameError] = useState(true);
+
+	function validationUsername(val) {
+		if (!val || val === '' || /^\s*$/.test(val)) {
+			setUsernameError(true);
+			setUsernameHelper("Username cannot be empty.")
+		}
+        else if (/\s/.test(val)) {
+            setUsernameError(true);
+			setUsernameHelper("Username cannot contain spaces.")
+        }
+		else{
+			setUsername(val);
+			setUsernameError(false);
+			setUsernameHelper("")
+		} 
+	}
+
+	function validationPassword(val) {
+		if (!val || val === '') {
+			setPasswordError(true);
+			setPasswordHelper("Password cannot be empty.")
+		} 
+        else if (/\s/.test(val)) {
+            setPasswordError(true);
+			setPasswordHelper("Password cannot contain spaces.")
+        }
+		else if (/^[0-9]*$/.test(val) || /^[a-zA-Z]*$/.test(val) || val.length < 8) {
+			setPasswordError(true);
+			setPasswordHelper("Password is not strong enough.")
+		}
+		else{
+			setPassword(val);
+			setPasswordError(false);
+			setPasswordHelper("")
+		}
+	}
+
+    return (
+        <div>
+            <Grid
+              style={{
+                minHeight: "100px",
+                width: "100%",
+                padding: "25px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                flexWrap: "wrap"
+              }}
+              spacing={0}
+              lg={12}
+              sm={12}
+              md={6}
+              xs={6}
+              container={true}
+              item={false}
+              form={false}
+            >
+                <div className={useStyles.root}>
+                <Grid item padding={2}>
+                  <Avatar src={'/imgs/logo.png'} className={useStyles.large} style={{
+                    margin: "10px",
+                    width: "150px",
+                    minHeight: "150px",
+                    "border-radius":"20px"
+                  }} />
+                </Grid>
+                
+                <Grid item paddingY={1}>
+                  <TextField
+					error={usernameError}
+					fullWidth
+					variant='filled'
+					color='primary'
+					type='string'
+					label='Username'
+                    onChange={(e) => validationUsername(e.target.value)}
+					helperText = {usernameHelper}
+				  />
+                </Grid>
+                
+                <Grid item paddingY={1}>
+                  <TextField
+					error={passwordError}
+					fullWidth
+					variant='filled'
+					color='primary'
+					type='string'
+					label='Password'
+                    onChange={(e) => validationPassword(e.target.value)}
+					helperText = {passwordHelper}
+				  />
+                </Grid>
+
+                <Grid item paddingY={1}>
+                  <Button
+					fullWidth
+					size='medium'
+					variant='contained'
+					color='primary'
+					disabled = {(usernameError || passwordError)}
+					//onClick={() => login function}
+                  >
+				    Login
+				  </Button>
+                </Grid>
+                </div>
+            </Grid>
+        </div>
+    );
+}  
+export default Login;
