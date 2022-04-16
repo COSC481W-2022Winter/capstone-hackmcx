a245bc20c437550aa06dac73b40e26c86cb7c057
ahassani93 <60075993+ahassani93@users.noreply.github.com>
2022-04-16 15:10:51 -0400

    enhancement: Implemented Upload Image in Create Post
    Cyrus and I have implemented an image upload button in Create Post that has replaced the image url textfield. Here when a user clicks on the button, it will open up their files where they can select the file they want to upload. Once selected and creating the post it will redirect them to the landing page where they will see their new post. There is a one mb limit to the image that they can upload.

--- packages/hackmcx-client/src/components/posts/CreatePost.js
+++ packages/hackmcx-client/src/components/posts/CreatePost.js
@@ -1,22 +1,35 @@
 import TextField from '@material-ui/core/TextField';
 import ImageIcon from '@mui/icons-material/Image';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@mui/material/Typography';
 import LinkIcon from '@mui/icons-material/Link';
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { FormControl } from '@material-ui/core';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+var B64;
 
 const CreatePost = () => {
 	const nav = useNavigate();
-
 	const [title, setTitle] = useState('');
-	const [imageUrl, setImageUrl] = useState('');
 	const [titleHelper, setTitleHelper] = useState('Please enter a valid title.');
-	const [urlHelper, setURLHelper] = useState('Image URL cannot be empty.');
-	const [imageUrlError, setImageUrlError] = useState(true);
+	const [uploadHelper, setUploadHelper] = useState('Must upload an image');
+	const [uploadError, setUploadError] = useState(true);
 	const [titleError, setTitleError] = useState(true);
+	const [selectedFile, setSelectedFile] = useState(false);
+	const [base64, setBase64] = useState('');
+
+	const Input = styled('input')({
+		display: 'none',
+	});
 
 	const token = localStorage.getItem('authToken');
 	let header = {
@@ -34,43 +47,75 @@ const CreatePost = () => {
 		}
 	}
 
-	function validationImageURL(val) {
-		if (!val || val === '') {
-			setImageUrlError(true);
-			setURLHelper('Image URL cannot be empty.');
-		} else if (!/(https?:\/\/.*\.(?:png|jpg|gif|svg))/i.test(val)) {
-			setImageUrlError(true);
-			setURLHelper('Image URL is not a valid URL');
+	function onFileChange(e) {
+		if (selectedFile != null) {
+			setUploadError(false);
+			setUploadHelper('');
+			setSelectedFile(e.target.files[0]);
+			var promise = getBase64(e.target.files[0]);
+			promise.then(function (result) {
+			B64 = result.slice(23, result.length - 1);
+			});
+			asyncCall(e.target.files[0]);
 		} else {
-			setImageUrl(val);
-			setImageUrlError(false);
-			setURLHelper('');
+			setUploadError(true);
+			setUploadError('Must upload an image');
 		}
 	}
 
-	//When the create post button is clicked, this function will be called.
-	function postRequest() {
+	async function asyncCall(img) {
+		const result = await getBase64(img);
+		setBase64(result);
+	}
+
+	function getBase64(file) {
+		return new Promise(function (resolve, reject) {
+			var reader = new FileReader();
+			reader.onload = function () {
+				resolve(reader.result);
+			};
+			reader.onerror = reject;
+			reader.readAsDataURL(file);
+		});
+	}
+
+	function onFileUpload() {
+		const formData = new FormData();
+
+		formData.append(
+			'myFile',
+			this.state.selectedFile,
+			this.state.selectedFile.name
+		);
+
+		console.log(this.state.selectedFile);
+	}
+
+	function postRequest(B64) {
 		axios
 			.post(
 				`${process.env.REACT_APP_API_URL}/api/v1/posts`,
 				{
+					imageData: B64,
 					title: title,
-					imageUrl: imageUrl,
 				},
 				header
 			)
 			.then(
 				(response) => {
 					console.log(response);
-					setImageUrlError(false);
 					setTitleError(false);
 				},
 				(error) => {
 					if (error == 'Error: Request failed with status code 401') {
 						alert('Unauthorized action, redirecting you to the Log in Page');
 						nav(`/login`);
-					} else {
-						alert('Caption could not be rated!');
+					}
+					else if(B64.length >= 1333336){
+						alert('Image size must be less than or equal to 1 mb!');
+					}
+					else {
+						alert('Post could not be created!');
 					}
 				}
 			);
@@ -85,7 +130,7 @@ const CreatePost = () => {
 			alignItems={'stretch'}>
 			<Grid item xs={7}>
 				<Grid container direction={'row'} alignItems={'center'}>
-					<Grid item xs={11}>
+					<Grid item xs={12}>
 						<ImageIcon fontSize='large' />
 						<TextField
 							error={titleError}
@@ -102,29 +147,42 @@ const CreatePost = () => {
 			</Grid>
 			<Grid item xs={7}>
 				<Grid container direction={'row'} spacing={2} alignItems={'center'}>
-					<Grid item xs={11}>
-						<LinkIcon fontSize='large' />
-						<TextField
-							error={imageUrlError}
-							fullWidth
-							variant='filled'
-							color='primary'
-							type='url'
-							label='Image URL'
-							onChange={(e) => validationImageURL(e.target.value)}
-							helperText={urlHelper}
-						/>
+					<Grid item xs={12}>
+						<Grid container direction={'row'} alignItems={'center'}>
+							<Grid item xs={12}>
+								<label htmlFor='contained-button-file'>
+									<Input
+										accept='image/*'
+										id='contained-button-file'
+										multiple
+										type='file'
+										helperText={uploadHelper}
+										onChange={(e) => onFileChange(e)}
+									/>
+									<Button
+										variant='contained'
+										fullWidth
+										component='span'
+										onClick={() => onFileUpload()}
+										color='secondary'>
+										Upload Image
+									</Button>
+									{selectedFile.name}
+									{uploadHelper}
+								</label>
+							</Grid>
+						</Grid>
 					</Grid>
 				</Grid>
 			</Grid>
-			<Grid item xs={6} align={'center'}>
+			<Grid item xs={7} align={'center'}>
 				<Button
 					fullWidth
 					size='medium'
 					variant='contained'
 					color='secondary'
-					disabled={titleError || imageUrlError}
-					onClick={() => postRequest()}
+					disabled={titleError || uploadError}
+					onClick={() => postRequest(B64)}
 					component={Link}
 					to='/'>
 					Create Post
