70a8db54ec6d0b9dc1240e2bf4fbcafd1952dca9
ckhorra1 <ckhorra1@emich.edu>
2022-04-17 17:54:04 -0400

    enhancement: Added upload image feature to account update page
    Ari and I have added an image upload button in the modal of the user profile page. This makes it so the user can upload an image from their local machine and replace their profile picture with it.

--- packages/hackmcx-client/src/components/UserProfile.js
+++ packages/hackmcx-client/src/components/UserProfile.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { useParams } from 'react-router-dom'
-import { CircularProgress } from "@material-ui/core";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@mui/material/Box';
@@ -13,172 +13,222 @@ import Avatar from '@mui/material/Avatar';
 import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ErrorPage from './ErrorPage';
-
-
-
+import { useForm } from 'react-hook-form';
 
 function UserId(Component) {
-
-    return function WrappedComponent(props) {
-      const {userId} = useParams();
-      return <Component {...props} userId={userId} />;
-    }
-  }
-
-
-  class UserProfile extends React.Component {
-
-    state = {
-        isLoaded: false,
-        user: null,
-        posts: [],
-        error: null,
-        searchQuery: ""
-    }
-
-    
-    
-    
-    componentDidMount() {
-        fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/${this.props.userId}`)
-            .then( res => res.json())
-            .then(
-                (result) => {
-                  this.setState({
-                    isLoaded: true,
-                    user: result
-                  });
-                },
-                (error) => {
-                  this.setState({
-                    isLoaded: true,
-                    error
-                  });
-                }
-            )
-    }
-
-    render() {
-        const style = {
-            position: "relative",
-            top: "50px"
-        }
-        
-        const { error, isLoaded, user, posts } = this.state;
-
-        const useStyles = makeStyles((theme) => ({
-          root: {
-            alignSelf: 'center',
-            justifyContent: "center",
-            alignItems: "center",
-            display: 'flex',
-            '& > *': {
-              margin: theme.spacing(1),
-            },
-          },
-          input: {
-            display: "none",
-          },
-          large: {
-            width: theme.spacing(7),
-            height: theme.spacing(7),
-          },
-          overlay:{
-            position: "absolute",
-            width: "100%",
-            opacity: 0.8,
-            height:"50%",
-            top:"50%",
-            transition: ".3s ease",
-            background: "red",
-          }
-
-        }));
-        
-
-
-        
-        if (error) return <ErrorPage />;
-        else if (!isLoaded) return <CircularProgress />;
-        else {
-            return (
-             
-                <div>
-                   
-                    <Grid
-    style={{
-      minHeight: "100px",
-      width: "100%",
-      padding: "25px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      flexWrap: "wrap"
-    }}
-    spacing={0}
-    lg={12}
-    sm={12}
-    md={6}
-    xs={6}
-    container={true}
-    item={false}
-    form={false}
-  >
-  
-
-<div className={useStyles.root}>
-
-  <IconButton color="primary" aria-label="upload picture" component="span" 
-  style={{ "border-radius":"20px",background:"rgb(160 3 24)"}}
-  // style={{background:"#a00318ad"}}
-  >
-    <Avatar src={user.imageUrl} className={useStyles.large} style={{
-      margin: "10px",
-      width: "150px",
-        minHeight: "150px",
-        "border-radius":"20px"
-     }} />
-  </IconButton>
-  {/* <Typography variant="p" gutterBottom component="div"  style={{ "position": "relative", background:"rgb(160 3 24 / 64%)",marginTop: "-70px", "z-index":200, "text-align":"center", color:"white", "paddingBottom":"15px"}}>
-    Change Image <br /> using update account
-      </Typography> */}
-</div>
-
-    <Typography variant="h6" gutterBottom component="div"  style={{ marginTop: "20px" }}>
-    username : {user.username} 
-      </Typography>
-
-    <Typography variant="h4" gutterBottom component="div"  style={{ marginTop: "10px" }}>
-    {user.first_name} {user.last_name}  
-      </Typography>
-    <Grid
-      style={{ minHeight: "100px", width: "100%", padding: "25px" }}
-      spacing={0}
-      lg={12}
-      sm={12}
-      md={12}
-      xs={12}
-      container={false}
-      item={true}
-      form={false}
-    >
-
-      <Demo user={user}/>
-     
-    </Grid>
-  </Grid>
-
-
-                    
-                </div>
-                 
-            );
-        }
-    }
-
+	return function WrappedComponent(props) {
+		const { userId } = useParams();
+		return <Component {...props} userId={userId} />;
+	};
 }
 
+var extraCharacter;
 
+class UserProfile extends React.Component {
+	state = {
+		isLoaded: false,
+		user: null,
+		posts: [],
+		error: null,
+		searchQuery: '',
+	};
+
+	componentDidMount() {
+		fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/${this.props.userId}`)
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					this.setState({
+						isLoaded: true,
+						user: result,
+					});
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error,
+					});
+				}
+			);
+	}
+	conditionCheck(user) {
+		if (user.imageData.slice(0, 1) == '/') {
+			extraCharacter = '';
+		} else {
+			extraCharacter = 'i';
+		}
+	}
+	render() {
+		const style = {
+			position: 'relative',
+			top: '50px',
+		};
+
+		const { error, isLoaded, user, posts } = this.state;
+
+		const useStyles = makeStyles((theme) => ({
+			root: {
+				alignSelf: 'center',
+				justifyContent: 'center',
+				alignItems: 'center',
+				display: 'flex',
+				'& > *': {
+					margin: theme.spacing(1),
+				},
+			},
+			input: {
+				display: 'none',
+			},
+			large: {
+				width: theme.spacing(7),
+				height: theme.spacing(7),
+			},
+			overlay: {
+				position: 'absolute',
+				width: '100%',
+				opacity: 0.8,
+				height: '50%',
+				top: '50%',
+				transition: '.3s ease',
+				background: 'red',
+			},
+		}));
+
+		if (error) return <ErrorPage />;
+		else if (!isLoaded) return <CircularProgress />;
+		else {
+			return (
+				<div>
+					<Grid
+						style={{
+							minHeight: '100px',
+							width: '100%',
+							padding: '25px',
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+							flexWrap: 'wrap',
+						}}
+						spacing={0}
+						lg={12}
+						sm={12}
+						md={6}
+						xs={6}
+						container={true}
+						item={false}
+						form={false}>
+						{/* <div className={useStyles.root}>
+							<IconButton
+								color='primary'
+								aria-label='upload picture'
+								component='span'
+								style={{ 'border-radius': '20px', background: 'rgb(160 3 24)' }}
+								// style={{background:"#a00318ad"}}
+							>
+								<Avatar
+									src={user.imageimage}
+									className={useStyles.large}
+									style={{
+										margin: '10px',
+										width: '150px',
+										minHeight: '150px',
+										'border-radius': '20px',
+									}}
+								/>
+							</IconButton>
+							{
+						</div> */}
+
+						<div className={useStyles.root}>
+							{/* <input
+								accept='image/*'
+								className={useStyles.input}
+								id='icon-button-file'
+								type='file'
+								hidden
+							/> */}
+							<label htmlFor='icon-button-file'>
+								<IconButton
+									color='primary'
+									aria-label='upload picture'
+									component='span'
+									style={{
+										'border-radius': '20px',
+										background: 'rgb(160 3 24)',
+									}}
+									// style={{background:"#a00318ad"}}
+								>
+									<Avatar
+										//Below works with Jpg only
+										// src={'data:image/png;base64,' + user.imageData}
+										//Works with png only\
+										{...this.conditionCheck(user)}
+										src={
+											'data:image/png;base64, ' +
+											extraCharacter +
+											user.imageData
+										}
+										{...console.log('data:image/png;base64, ' + user.imageData)}
+										className={useStyles.large}
+										style={{
+											margin: '10px',
+											width: '150px',
+											minHeight: '150px',
+											'border-radius': '20px',
+										}}
+									/>
+								</IconButton>
+								{/* <Typography
+									variant='p'
+									gutterBottom
+									component='div'
+									style={{
+										position: 'relative',
+										background: 'rgb(160 3 24 / 64%)',
+										marginTop: '-50px',
+										'z-index': 200,
+										'text-align': 'center',
+										color: 'white',
+										paddingBottom: '15px',
+									}}>
+									Change Image
+								</Typography> */}
+							</label>
+						</div>
+
+						<Typography
+							variant='h6'
+							gutterBottom
+							component='div'
+							style={{ marginTop: '20px' }}>
+							username : {user.username}
+						</Typography>
+
+						<Typography
+							variant='h4'
+							gutterBottom
+							component='div'
+							style={{ marginTop: '10px' }}>
+							{user.first_name} {user.last_name}
+						</Typography>
+						<Grid
+							style={{ minHeight: '100px', width: '100%', padding: '25px' }}
+							spacing={0}
+							lg={12}
+							sm={12}
+							md={12}
+							xs={12}
+							container={false}
+							item={true}
+							form={false}>
+							<Demo user={user} />
+						</Grid>
+					</Grid>
+				</div>
+			);
+		}
+	}
+}
 
 export default UserId(UserProfile);
--- packages/hackmcx-client/src/components/updateAccountModal.js
+++ packages/hackmcx-client/src/components/updateAccountModal.js
@@ -1,267 +1,411 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Modal from "@material-ui/core/Modal";
-import Button from "@material-ui/core/Button";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
+import { styled } from '@mui/material/styles';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 // function rand() {
 //   return Math.round(Math.random() * 20) - 10;
 // }
+var B64;
+var extraChar;
 
 function getModalStyle() {
-  const top = 50 ;
-  const left = 50 ;
+	const top = 50;
+	const left = 50;
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
-  };
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
 }
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(4),
-    outline: "none"
-  }
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(4),
+		outline: 'none',
+	},
 }));
 
 function SimpleModal(props) {
-  const [open, setOpen] = useState(false);
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = useState(getModalStyle);
-  const [modalData, setData] = useState();
-  const navigate = useNavigate();
+	const [open, setOpen] = useState(false);
+	// getModalStyle is not a pure function, we roll the style only on the first render
+	const [modalStyle] = useState(getModalStyle);
+	const [modalData, setData] = useState();
+	const navigate = useNavigate();
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-//const onSubmit = data => console.log(data);
-const onSubmit = async (data) => {
-  const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json',
-      "Authorization": "Bearer "+ localStorage.getItem("authToken")+""},
-      body: JSON.stringify(data)
-  };
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	//const onSubmit = data => console.log(data);
+	const onSubmit = async (data) => {
+		// if (selectedFile.type == 'image/png') {
+		// 	B64 = extraChar + B64.slice(0, B64.length - 2);
+		// }
+		if (B64.length >= 1333336) {
+			alert('Image size must be less than or equal to 1 mb!');
+		}
 
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/${props.user.username}`, requestOptions);
-  const jsonData = await response.status;
+		const requestOptions = {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('authToken') + '',
+			},
+			body: JSON.stringify(data),
+		};
+		const response = await fetch(
+			`${process.env.REACT_APP_API_URL}/api/v1/users/${props.user.username}`,
+			requestOptions
+		);
+		const jsonData = await response.status;
 
-  console.log(jsonData);
-  window.location.href=window.location.href
-  navigate(`/user/${props.user.username}`);
-}
+		console.log(jsonData);
+		window.location.href = window.location.href;
+		navigate(`/user/${props.user.username}`);
+	};
 
+	const data = [
+		{
+			title: 'Update Account Information',
+			Info: 'Update Account Information',
+		},
+	];
+	const [title, setTitle] = useState('');
+	const [titleHelper, setTitleHelper] = useState('Please enter a valid title.');
+	const [uploadHelper, setUploadHelper] = useState('No image selected');
+	const [uploadError, setUploadError] = useState(true);
+	const [titleError, setTitleError] = useState(true);
+	const [selectedFile, setSelectedFile] = useState(false);
+	const [base64, setBase64] = useState('');
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
 
-  const data = [
-    {
-      title: "Update Account Information",
-      Info: "Update Account Information"
-    },
-  ];
-  const CustomModal = () => {
-    if(localStorage.getItem("authToken")!=null && localStorage.getItem("tokenExpires")>Date.now()){
-    return modalData ? (
-      <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={open}
-        onClose={handleClose}
-      >
-        <div style={modalStyle} className={classes.paper}>
-          
-  <Grid
-    style={{ minHeight: "100px", width: "100%", padding: "25px" }}
-    spacing={0}
-    lg={12}
-    sm={12}
-    md={12}
-    xs={12}
-    container={false}
-    item={true}
-    form={false}
-  >
-     <Typography variant="h6" id="modal-title">
-            {modalData.Info}
-          </Typography>
-    <Typography
-      style={{}}
-      variant={"h6"}
-      text={"Text"}
-      noWrap={false}
-      paragraph={false}
-      color={"textSecondary"}
-      align={"center"}
-      gutterBottom={false}
-    />
-    <form onSubmit={handleSubmit(onSubmit)}>
-    <TextField
-      style={{}}
-      fullWidth={true}
-      placeholder={"Doe"}
-      margin={"normal"}
-      label={"username"}
-      //disabled
-      name={"username"}
-      color={"primary"}
-      InputProps={{
-        readOnly: true,
-      }}
+	//////////////////////////SPRINT 4 CODE//////////////////////////////////
 
-      multiline={false}
-      rows={1}
-      rowsMax={1}
-      type={"text"}
-      variant={"filled"}
-      //onChange={event => setValue(event.target.value)}
-      {...register("username")}
-      value={props.user.username}
+	///////////////////////////POST REQUEST CODE ////////////////////
+	const nav = useNavigate();
 
-    />
-    <TextField
-      style={{}}
-      fullWidth={true}
-      helperText={"Enter/Edit your first name"}
-      placeholder={"Doe"}
-      margin={"normal"}
-      label={"User First Name"}
-      name={"firstname"}
-      color={"primary"}
-      multiline={false}
-      rows={1}
-      rowsMax={1}
-      type={"text"}
-      variant={"filled"}
-      //onChange={event => setValue(event.target.value)}
-      defaultValue={props.user.first_name}
-      {...register("firstname")}
+	const token = localStorage.getItem('authToken');
+	let header = {
+		headers: { Authorization: 'Bearer ' + token },
+	};
+	function postRequest(B64, extraChar) {
+		// if (selectedFile.type == 'image/png') {
+		// 	B64 = extraChar + B64.slice(0, B64.length - 2);
+		// }
+		if (B64.length >= 1333336) {
+			alert('Image size must be less than or equal to 1 mb!');
+		}
+		axios
+			.post(
+				`${process.env.REACT_APP_API_URL}/api/v1/posts`,
+				{
+					username: 'something',
+					first_name: firstName,
+					last_name: lastName,
+					imageData: B64,
+				},
+				header
+			)
+			.then(
+				(response) => {
+					console.log(response);
+					setTitleError(false);
+				},
+				(error) => {
+					if (error == 'Error: Request failed with status code 401') {
+						alert('Unauthorized action, redirecting you to the Log in Page');
+						nav(`/login`);
+					} else if (B64.length >= 1333336) {
+						alert('Image size must be less than or equal to 1 mb!');
+					} else {
+						alert('Post could not be created!');
+					}
+				}
+			);
+	}
 
+	const Input = styled('input')({
+		display: 'none',
+	});
 
-    />
-    <TextField
-      style={{}}
-      fullWidth={true}
-      helperText={"Enter/Edit your last name"}
-      placeholder={"Doe"}
-      margin={"normal"}
-      label={"User Last Name"}
-      name={"last_name"}
-      color={"primary"}
-      multiline={false}
-      rows={1}
-      rowsMax={1}
-      type={"text"}
-      variant={"filled"}
-      defaultValue={props.user.last_name}
-      {...register("lastname")}
+	function onFileChange(e) {
+		if (selectedFile != null) {
+			setUploadError(false);
+			setUploadHelper('');
+			setSelectedFile(e.target.files[0]);
+			var promise = getBase64(e.target.files[0]);
+			promise.then(function (result) {
+				extraChar = result.slice(22, 23);
+				B64 = result.slice(23, result.length);
+				//	B64 = result;
 
-    />
-    <TextField
-      style={{}}
-      fullWidth={true}
-      helperText={"Enter/Edit the link to your profile picture"}
-      placeholder={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/MJd1-.svg/100px-MJd1-.svg.png"}
-      margin={"normal"}
-      label={"Profile Picture URL"}
-      name={"imageUrl"}
-      color={"primary"}
-      multiline={false}
-      rows={1}
-      rowsMax={1}
-      type={"text"}
-      variant={"filled"}
-      defaultValue={props.user.imageUrl}
-      {...register("imageUrl")}
+				// var data;
+				const fromDb = undefined;
 
-    />
-    <TextField
-      style={{}}
-      fullWidth={true}
-      helperText={"Enter new password"}
-      placeholder={""}
-      margin={"normal"}
-      label={"password"}
-      name={"password"}
-      color={"primary"}
-      multiline={false}
-      rows={1}
-      rowsMax={1}
-      type={"password"}
-      variant={"filled"}
-      //value={props.user.last_name}
-      //onChange={handleInputChanged(props.user.imageUrl)}
-      required
-      {...register("password")}
+				// ✅ Objects
+				const obj = fromDb || {};
+				obj.firstname = `${props.user.first_name}`;
+				obj.lastname = `${props.user.last_name}`;
+				obj.imageData = B64;
 
-    />
-    <Button
-      style={{}}
-      variant={"contained"}
-      color={"primary"}
-      size={"large"}
-      text={"Update"}
-      fullWidth={true}
-      formSubmit={true}
-      align={"center"}
-      link={""}
-      isIcon={false}
-      type="submit"
-    >
-      Update Details
-    </Button>
-    </form>
-  </Grid>          
-          <div>
-        </div>
+				const requestOptions = {
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: 'Bearer ' + localStorage.getItem('authToken') + '',
+					},
+					body: JSON.stringify(obj),
+				};
 
-        </div>
-      </Modal>
-    ) : null;
-    }
-    else{
-      return modalData?(<Alert severity="error">Please login to update account details!</Alert>):null;
-    }
-  };
+				const response = fetch(
+					`${process.env.REACT_APP_API_URL}/api/v1/users/${props.user.username}`,
+					requestOptions
+				);
+				const jsonData = response.status;
 
-  const handleOpen = index => {
-    setOpen(true);
-    setData(data[index]);
-  };
+				console.log('FORMIMAHE' + jsonData);
+			});
+			asyncCall(e.target.files[0]);
+		} else {
+			setUploadError(true);
+			setUploadError('No image selected');
+		}
+	}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const classes = useStyles();
+	async function asyncCall(img) {
+		const result = await getBase64(img);
+		setBase64(result);
+	}
 
-  return (
-    <div>
-      {data.map((d, index) => (
-        <div>
-          <Button
-        style={{ marginBottom: "10px" }}
-        variant={"contained"}
-         color={"primary"}
-        size={"medium"}
-        text={"Edit Account Details"}
-        fullWidth={true}
-        formSubmit={false}
-        align={"center"}
-        onClick={() => {
-          handleOpen(index);
-        }}
-      >
-        {d.title}
-      </Button>
+	function getBase64(file) {
+		return new Promise(function (resolve, reject) {
+			var reader = new FileReader();
+			reader.onload = function () {
+				resolve(reader.result);
+			};
+			reader.onerror = reject;
+			reader.readAsDataURL(file);
+		});
+	}
 
-      {/* <Button
+	function onFileUpload() {
+		const formData = new FormData();
+
+		formData.append(
+			'myFile',
+			this.state.selectedFile,
+			this.state.selectedFile.name
+		);
+
+		console.log(this.state.selectedFile);
+	}
+
+	const CustomModal = () => {
+		if (
+			localStorage.getItem('authToken') != null &&
+			localStorage.getItem('tokenExpires') > Date.now()
+		) {
+			return modalData ? (
+				<Modal
+					aria-labelledby='simple-modal-title'
+					aria-describedby='simple-modal-description'
+					open={open}
+					onClose={handleClose}>
+					<div style={modalStyle} className={classes.paper}>
+						<Grid
+							style={{ minHeight: '100px', width: '100%', padding: '25px' }}
+							spacing={0}
+							lg={12}
+							sm={12}
+							md={12}
+							xs={12}
+							container={false}
+							item={true}
+							form={false}>
+							<Typography variant='h6' id='modal-title'>
+								{modalData.Info}
+							</Typography>
+							<Typography
+								style={{}}
+								variant={'h6'}
+								text={'Text'}
+								noWrap={false}
+								paragraph={false}
+								color={'textSecondary'}
+								align={'center'}
+								gutterBottom={false}
+							/>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								{/* <TextField
+									style={{}}
+									fullWidth={true}
+									placeholder={'Doe'}
+									margin={'normal'}
+									label={'username'}
+									//disabled
+									name={'username'}
+									color={'primary'}
+									InputProps={{
+										readOnly: true,
+									}}
+									multiline={false}
+									rows={1}
+									rowsMax={1}
+									type={'text'}
+									variant={'filled'}
+									//onChange={event => setValue(event.target.value)}
+									{...register('username')}
+									value={props.user.username}
+								/> */}
+								<TextField
+									style={{}}
+									fullWidth={true}
+									helperText={'Enter/Edit your first name'}
+									// placeholder={'Doe'}
+									margin={'normal'}
+									label={'User First Name'}
+									name={'firstname'}
+									color={'primary'}
+									multiline={false}
+									rows={1}
+									rowsMax={1}
+									type={'text'}
+									variant={'filled'}
+									//////////////////////////////////////////////
+									onChange={(e) => setFirstName(e.target.value)}
+									//////////////////////////////////////////////
+									//onChange={event => setValue(event.target.value)}
+									defaultValue={props.user.first_name}
+									{...register('firstname')}
+								/>
+								<TextField
+									style={{}}
+									fullWidth={true}
+									helperText={'Enter/Edit your last name'}
+									// placeholder={'Doe'}
+									margin={'normal'}
+									label={'User Last Name'}
+									name={'last_name'}
+									color={'primary'}
+									multiline={false}
+									rows={1}
+									rowsMax={1}
+									type={'text'}
+									variant={'filled'}
+									defaultValue={props.user.last_name}
+									//////////////////////////////////////////////
+									onChange={(e) => setLastName(e.target.value)}
+									//////////////////////////////////////////////
+
+									{...register('lastname')}
+								/>
+								<label htmlFor='contained-button-file'>
+									<Input
+										accept='image/*'
+										id='contained-button-file'
+										multiple
+										type='file'
+										helperText={uploadHelper}
+										onChange={(e) => onFileChange(e)}
+										// {...register('photo')}
+									/>
+									<Button
+										variant='contained'
+										fullWidth
+										component='span'
+										onClick={() => onFileUpload()}
+										color='secondary'>
+										Upload Image
+									</Button>
+									{selectedFile.name}
+									{uploadHelper}
+								</label>
+								{/* <input
+									type='file'
+									onChange={(e) => onFileChange(e)}
+									{...register('picture')}
+								/> */}
+								{/* <input {...register('picture')} type='file' />{' '} */}
+
+								<Button
+									style={{}}
+									variant={'contained'}
+									color={'secondary'}
+									size={'large'}
+									text={'Update'}
+									fullWidth={true}
+									formSubmit={true}
+									align={'center'}
+									link={''}
+									isIcon={false}
+									//////////////////////////////////////////////
+									//onClick={() => postRequest(B64)}
+									//////////////////////////////////////////////
+
+									type='submit'>
+									Update Details
+								</Button>
+							</form>
+						</Grid>
+						<div></div>
+					</div>
+				</Modal>
+			) : null;
+		} else {
+			return modalData ? (
+				<Alert severity='error'>Please login to update account details!</Alert>
+			) : null;
+		}
+	};
+
+	const handleOpen = (index) => {
+		setOpen(true);
+		setData(data[index]);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const classes = useStyles();
+
+	return (
+		<div>
+			{data.map((d, index) => (
+				<div>
+					<Button
+						style={{ marginBottom: '10px' }}
+						variant={'contained'}
+						color={'secondary'}
+						size={'medium'}
+						text={'Edit Account Details'}
+						fullWidth={true}
+						formSubmit={false}
+						align={'center'}
+						onClick={() => {
+							handleOpen(index);
+						}}>
+						{d.title}
+					</Button>
+
+					{/* <Button
         style={{}}
         variant={"contained"}
         color={"primary"}
@@ -278,18 +422,18 @@ const onSubmit = async (data) => {
         Change Profile Picture 
       </Button> */}
 
-          {/* <Button
+					{/* <Button
             onClick={() => {
               handleOpen(index);
             }}
           >
             {d.title}
           </Button> */}
-        </div>
-      ))}
-      <CustomModal />
-    </div>
-  );
+				</div>
+			))}
+			<CustomModal />
+		</div>
+	);
 }
 
 export default SimpleModal;
